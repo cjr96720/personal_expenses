87cd6f91d5ed519dd7676b9e9ddc217028d21cd4
cjr96720 <cjr96720@umd.edu>
2021-08-12 22:01:56 +0800

    Working on the main interface. Error needed to be solved: cannot run with sound null safety...

---
+++ lib/transaction.dart
@@ -0,0 +1,14 @@
+// custom class
+class Transaction {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime? date;
+
+  Transaction({
+    required this.id,
+    required this.title,
+    required this.amount,
+    required this.date,
+  });
+}
